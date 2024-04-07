b5f5e7ec633b56100aef2c799ba09656e1b87fe5
Amit kumar jaiswal <amit.sms3011@gmail.com>
2024-04-07 11:31:29 +0530

    127:Create Save Post Featcher

---
+++ src/main/java/com/myblog/myblog24/Myblog24Application.java
@@ -0,0 +1,13 @@
+package com.myblog.myblog24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblog24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myblog24Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/myblog/myblog24/Myblog24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblog.myblog24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myblog24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
