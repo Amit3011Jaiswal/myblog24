ad6606d5321eab92f23655c17e5561a2acdbd355
Amit kumar jaiswal <amit.sms3011@gmail.com>
2024-04-20 11:32:22 +0530

    127:Create Save Post Featcher Entity Added 4jan 2024 Add ResouceNotFound

--- src/main/java/com/myblog/myblog24/service/PostService.java
+++ src/main/java/com/myblog/myblog24/service/PostService.java
@@ -2,7 +2,15 @@ package com.myblog.myblog24.service;
 
 import com.myblog.myblog24.payload.PostDto;
 
+import java.util.List;
+
 public interface PostService  {
 
     PostDto createPost(PostDto postDto);
+
+
+    PostDto getPostById(long id);
+
+
+    List<PostDto> getAllPosts(int pageNo, int pageSize);
 }
--- src/main/java/com/myblog/myblog24/service/impl/PostServiceImpl.java
+++ src/main/java/com/myblog/myblog24/service/impl/PostServiceImpl.java
@@ -5,7 +5,13 @@ import com.myblog.myblog24.exception.ResourceNotFoundException;
 import com.myblog.myblog24.payload.PostDto;
 import com.myblog.myblog24.repository.PostRepository;
 import com.myblog.myblog24.service.PostService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PostServiceImpl implements PostService {
@@ -18,16 +24,11 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public PostDto createPost(PostDto postDto) {
-        Post post = new Post();
-        post.setTitle(postDto.getTitle());
-        post.setDescription(postDto.getDescription());
-        post.setContent(postDto.getContent());
+
+        Post post = mapToEntity(postDto);
         Post savedPost = postRepository.save(post);
 
-        PostDto dto = new PostDto();
-        dto.setTitle(savedPost.getTitle());
-        dto.setDescription(savedPost.getDescription());
-        dto.setContent(savedPost.getContent());
+        PostDto dto = mapToDto(savedPost);
         return dto;
     }
 
@@ -43,5 +44,33 @@ public class PostServiceImpl implements PostService {
         dto.setDescription(post.getDescription());
         dto.setContent(post.getContent());
         return dto;
+    }
+
+    @Override
+    public List<PostDto> getAllPosts(int pageNo, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        Page<Post> pagePost = postRepository.findAll(pageable);
+        List<Post> posts = pagePost.getContent();
+        List<PostDto> dtos = posts.stream().map(p -> mapToDto(p)).collect(Collectors.toList());
+        return dtos;
+    }
+
+
+
+    PostDto mapToDto(Post post){
+        PostDto dto = new PostDto();
+        dto.setId(post.getId());
+        dto.setTitle(post.getTitle());
+        dto.setDescription(post.getDescription());
+        dto.setContent(post.getContent());
+        return dto;
+    }
+
+    Post mapToEntity(PostDto postDto){
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+        return post;
     }
 }
